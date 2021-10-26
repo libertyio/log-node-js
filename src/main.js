b136d39163d94a64e8b4457b2c988584bd869b84
Jonathan Buhacoff <jonathan@buhacoff.net>
2021-10-26 10:54:23 -0700

    fix: convert to es6 module

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 Copyright (C) 2020 Liberty Infrasystems LLC. All rights reserved.
 */
 
-const colors = require('colors');
+import colors from 'colors';
 
 /*
 The `enable` option controls which methods produce output -- default is all of them. To
@@ -28,7 +28,7 @@ stream:
     possible values include process.stderr and process.stdout
 
 withLevel:
-    error, warn, info, ok, trace: '[c2] <level>'
+    error, warn, info, ok, trace: '[tag] <level>'
     print: none
 
 newlines:
@@ -41,21 +41,61 @@ newlines:
 class Log {
     constructor(options = {}) {
         const {
-            tag = null, withLevel = true, enable = {
-                error: true, warn: true, info: true, ok: true, trace: true, print: true,
-            }, /* color = null, */ stream = process.stderr,
+            tag = null,
+            withLevel = true,
+            level = 'info',
+            enable = {}, // can set any of 'error', 'warn', 'info', 'ok', 'trace', 'print' here to override level defaults
+            /* color = null, */
+            stream = process.stderr,
         } = options;
 
-        const formatWithTagLevelText = function formatWithTagLevelText(level, text) {
-            return `[${tag}] ${level}: ${text}\n`;
+        let enabled;
+
+        switch (level) {
+        case 'error':
+            enabled = {
+                error: true, warn: false, ok: false, info: false, trace: false, print: true, ...enable,
+            };
+            break;
+        case 'warn':
+            enabled = {
+                error: true, warn: true, ok: false, info: false, trace: false, print: true, ...enable,
+            };
+            break;
+        case 'ok':
+            enabled = {
+                error: true, warn: true, ok: true, info: false, trace: false, print: true, ...enable,
+            };
+            break;
+        case 'info':
+            enabled = {
+                error: true, warn: true, ok: true, info: true, trace: false, print: true, ...enable,
+            };
+            break;
+        case 'trace':
+            enabled = {
+                error: true, warn: true, ok: true, info: true, trace: true, print: true, ...enable,
+            };
+            break;
+        default:
+            // if level is invalid, default to info and display a warning
+            enabled = {
+                error: true, warn: true, ok: true, info: true, trace: false, print: true, ...enable,
+            };
+            stream.write(colors.yellow(`LOG: invalid log level selected: ${level}\n`));
+            break;
+        }
+
+        const formatWithTagLevelText = function formatWithTagLevelText(textLevel, text) {
+            return `[${tag}] ${textLevel}: ${text}\n`;
         };
-        const formatWithTagText = function formatWithTagText(level, text) {
+        const formatWithTagText = function formatWithTagText(textLevel, text) {
             return `[${tag}] ${text}\n`;
         };
-        const formatWithLevelText = function formatWithLevelText(level, text) {
-            return `${level}: ${text}\n`;
+        const formatWithLevelText = function formatWithLevelText(textLevel, text) {
+            return `${textLevel}: ${text}\n`;
         };
-        const formatWithText = function formatWithText(level, text) {
+        const formatWithText = function formatWithText(textLevel, text) {
             return `${text}\n`;
         };
 
@@ -76,39 +116,59 @@ class Log {
             printFormat = formatWithText;
         }
 
-        this.error = function error(arg) {
-            if (enable.error) {
+        const traceExtraParams = (params) => {
+            if (Array.isArray(params)) {
+                for (let i = 0; i < params.length; i += 1) {
+                    if (typeof params[i] === 'object' && typeof params[i].stack === 'string') {
+                        // stream.write(colors.gray(format('trace', `[${i+1}] ${params[i].stack}`)));
+                        this.trace(`[${i + 1}] ${params[i].stack}`);
+                    } else {
+                        // stream.write(colors.gray(format('trace', `[${i+1}] ${JSON.stringify(params[i])}`)));
+                        this.trace(`[${i + 1}] ${JSON.stringify(params[i])}`);
+                    }
+                }
+            }
+        };
+
+        this.error = function error(arg, ...params) {
+            if (enabled.error) {
                 stream.write(colors.red(format('error', arg)));
+                traceExtraParams(params);
             }
         };
 
-        this.warn = function warn(arg) {
-            if (enable.warn) {
+        this.warn = function warn(arg, ...params) {
+            if (enabled.warn) {
                 stream.write(colors.yellow(format('warn', arg)));
+                traceExtraParams(params);
             }
         };
 
-        this.info = function info(arg) {
-            if (enable.info) {
-                stream.write(colors.blue(format('info', arg)));
-            }
-        };
-
-        this.ok = function ok(arg) {
-            if (enable.ok) {
+        this.ok = function ok(arg, ...params) {
+            if (enabled.ok) {
                 stream.write(colors.green(format('ok', arg)));
+                traceExtraParams(params);
             }
         };
 
-        this.trace = function trace(arg) {
-            if (enable.trace) {
+        this.info = function info(arg, ...params) {
+            if (enabled.info) {
+                stream.write(colors.blue(format('info', arg)));
+                traceExtraParams(params);
+            }
+        };
+
+        this.trace = function trace(arg, ...params) {
+            if (enabled.trace) {
                 stream.write(colors.gray(format('trace', arg)));
+                traceExtraParams(params);
             }
         };
 
-        this.print = function print(arg) {
-            if (enable.print) {
+        this.print = function print(arg, ...params) {
+            if (enabled.print) {
                 stream.write(printFormat(null, arg));
+                traceExtraParams(params);
             }
         };
     }
--- test/log.js
+++ test/log.js
@@ -1,4 +1,4 @@
-const { Log } = require('../dist/main.umd.js');
+import { Log } from '../src/main.js';
 
 describe('log', function () {
     it('prints nicely by default', function () {
@@ -6,8 +6,8 @@ describe('log', function () {
         // log to stderr
         log.error('foo');
         log.warn('foo');
-        log.info('foo');
         log.ok('foo');
+        log.info('foo');
         log.trace('foo');
         log.print('foo'); // except for this one        
     });
@@ -16,8 +16,8 @@ describe('log', function () {
         // log to stderr
         log.error('foo');
         log.warn('foo');
-        log.info('foo');
         log.ok('foo');
+        log.info('foo');
         log.trace('foo');
         log.print('foo'); // including this one        
     });
@@ -25,8 +25,8 @@ describe('log', function () {
         const logOnlyTrouble = new Log({ enable: { error: true, warn: true }});
         logOnlyTrouble.error('foo');
         logOnlyTrouble.warn('foo');
-        logOnlyTrouble.info('foo'); // silent
         logOnlyTrouble.ok('foo');  // silent
+        logOnlyTrouble.info('foo'); // silent
         logOnlyTrouble.trace('foo');  // silent
         logOnlyTrouble.print('foo'); // silent
     });
@@ -34,9 +34,87 @@ describe('log', function () {
         const log = new Log({ tag: 'stdout', stream: process.stdout });
         log.error('foo');
         log.warn('foo');
-        log.info('foo');
         log.ok('foo');
+        log.info('foo');
         log.trace('foo');
         log.print('foo');
+    });
+    it('enables trace and above', function () {
+        const log = new Log({ tag: 'test', level: 'trace' });
+        // log to stderr
+        log.error('foo');
+        log.warn('foo');
+        log.ok('foo');
+        log.info('foo');
+        log.trace('foo');
+        log.print('foo'); // including this one        
+    });
+    it('enables info and above', function () {
+        const log = new Log({ tag: 'test', level: 'info' });
+        // log to stderr
+        log.error('foo');
+        log.warn('foo');
+        log.ok('foo');
+        log.info('foo');
+        log.trace('foo'); // not this one
+        log.print('foo'); // including this one        
+    });
+    it('enables info and above without print', function () {
+        const log = new Log({ tag: 'test', level: 'info', enable: { print: false } });
+        // log to stderr
+        log.error('foo');
+        log.warn('foo');
+        log.ok('foo');
+        log.info('foo');
+        log.trace('foo'); // not this one
+        log.print('foo'); // not this one        
+    });
+    it('enables warn and above', function () {
+        const log = new Log({ tag: 'test', level: 'warn' });
+        // log to stderr
+        log.error('foo');
+        log.warn('foo');
+        log.ok('foo'); // not this one
+        log.info('foo'); // not this one
+        log.trace('foo'); // not this one
+        log.print('foo'); // including this one        
+    });
+    it('enables error and above', function () {
+        const log = new Log({ tag: 'test', level: 'error' });
+        // log to stderr
+        log.error('foo');
+        log.warn('foo'); // not this one
+        log.ok('foo'); // not this one
+        log.info('foo'); // not this one
+        log.trace('foo'); // not this one
+        log.print('foo'); // including this one        
+    });
+    it('prints extra params when trace enabled', function () {
+        const log = new Log({ tag: 'test', level: 'trace' });
+        // log to stderr
+        try {
+            throw new Error('foo');
+        } catch (err) {
+            log.error('failed', err);
+            log.warn('failed', err);
+            log.ok('failed', err);
+            log.info('failed', err);
+            log.trace('failed', err);
+            log.print('failed', err); // including this one        
+        }
+    });
+    it('does not extra params when trace disabled', function () {
+        const log = new Log({ tag: 'test', level: 'info' });
+        // log to stderr
+        try {
+            throw new Error('foo');
+        } catch (err) {
+            log.error('failed', err); // the error, but not the trace
+            log.warn('failed', err); // the error, but not the trace
+            log.ok('failed', err); // the error, but not the trace
+            log.info('failed', err); // the error, but not the trace
+            log.trace('failed', err); // nothing
+            log.print('failed', err); // the message, but not the trace
+        }
     });
 });
